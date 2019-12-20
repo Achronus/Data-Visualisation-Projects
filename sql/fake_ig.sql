f7620abf27043b811b6fb444f5afcaa6a6fe3df4
Ryan Partridge <rpartridge101@yahoo.co.uk>
2019-12-20 14:08:59 +0000

    Updated project
    Added triggers and unfollows table

--- sql/fake_ig.sql
+++ sql/fake_ig.sql
@@ -51,6 +51,16 @@ CREATE TABLE follows
   PRIMARY KEY (follower_id, followee_id)
 );
 
+CREATE TABLE unfollows
+(
+  follower_id INT NOT NULL,
+  followee_id INT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (follower_id) REFERENCES users(id),
+  FOREIGN KEY (followee_id) REFERENCES users(id),
+  PRIMARY KEY (follower_id, followee_id)
+);
+
 CREATE TABLE tags
 (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -137,3 +147,27 @@ INNER JOIN likes
   ON users.id = likes.user_id
 GROUP BY likes.user_id
 HAVING num_likes = (SELECT COUNT(*) FROM photos);
+
+/* Preventing self-follows */
+DELIMITER $$
+CREATE TRIGGER prevent_self_follows
+	BEFORE INSERT ON follows FOR EACH ROW
+	BEGIN
+		IF NEW.follower_id = NEW.followee_id THEN 
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You cannot follow yourself!';
+    END IF;
+	END;
+$$
+DELIMITER ;
+
+/* Logging unfollows */
+DELIMITER $$
+CREATE TRIGGER capture_unfollow
+	AFTER DELETE ON follows FOR EACH ROW
+	BEGIN
+		INSERT INTO unfollows
+    SET follower_id = OLD.follower_id,
+        followee_id = OLD.followee_id;
+	END;
+$$
+DELIMITER ;
